6d79781c583e0a4cceede98059b418a9b52dcd72
Vaclav Bohac <bohac.v@gmail.com>
2011-05-01 15:51:09 +0200

    Removed perror when not neccessary.

--- server-processes/process.c
+++ server-processes/process.c
@@ -13,15 +13,15 @@ void process(int client, struct sockaddr_in* clientAddr)
 	size_t termlen = strlen(TERMINATION);
 	while (1) {
 		if (get_request(request, client) == -1) {
-			perror("Error while receiving message.");
+			fprintf(stderr, "Error while receiving message.\n");
 			break;
 		}
 		if (message_from_string(msg, request) == -1) {
-			perror("Error when parsing request.");
+			fprintf(stderr, "Error when parsing request.\n");
 			break;
 		}
 		if (handle_request(msg) == -1) {
-			perror("Error when handling request.");
+			fprintf(stderr, "Error when handling request.\n");
 			break;
 		}
 	}
@@ -34,15 +34,15 @@ int handle_request(struct message* msg)
 {
 	switch (msg->type) {
 		case 'C':
-			printf("Proccessing command with code: %d (%s).\n", msg->code, msg->text);
+			printf("Processing command with code: %d (%s).\n", msg->code, msg->text);
 			return 0;
 		break;
 		case 'I':
-			printf("Proccessing command with code: %d (%s).\n", msg->code, msg->text);
+			printf("Processing command with code: %d (%s).\n", msg->code, msg->text);
 			return 0;
 		break;
 		default:
-			perror("Invalid message type.");
+			fprintf(stderr, "Invalid message type.\n");
 			return -1;
 	}
 }
@@ -55,12 +55,12 @@ int get_request(char* request, int client)
 	length = read(client, buffer, bsize);
 	if (length == 0) {
 		request = NULL;
-		perror("Client closed connection.");
+		fprintf(stderr, "Client closed connection.\n");
 		return -1;
 	}
 	if (length == -1) {
 		request = NULL;
-		perror("Could not read from client.");
+		fprintf(stderr, "Could not read from client.\n");
 		return -1;
 	}
 	do {
