277c1f96ea06bfb126d27440f1e72a5d73878e94
Vaclav Bohac <bohac.v@gmail.com>
2011-05-05 21:01:06 +0200

    Threads/barber: added to main file

--- server-threads/main.c
+++ server-threads/main.c
@@ -1,11 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
+#include "barber.h"
 #include "tools/procargs.h"
 #include "protocol/server.h"
+#include "semaphores/sems.h"
+#include "shmemory/shared.h"
 
 #define DEFAULT_PORT 8080
+
+void cut_hair(int time)
+{
+	printf("Barber: Cutting hair for %d seconds.\n", time);
+	sleep(time);
+	printf("Barber: Hair cutted.\n");
+}
+
+void *barber(void *args)
+{
+	// Set shared memory and semaphores.
+	struct shared* data = get_shared(getuid());
+	data->semaphores = semaphores_init(getuid());
+	if (data->semaphores == -1) {
+		perror("Semaphores initialization error");
+		pthread_exit((void *) 1);
+	}
+	// Set customers to the default state.
+	data->custommers = 0;
+	while (1) {
+#ifdef DEBUG
+		printf("Barber: Custommers down.\n");
+#endif
+		// Wait for custommer.
+		down(data->semaphores, SEM_CUSTOMMERS);
+		// Enter critical section.
+		down(data->semaphores, SEM_MUTEX);
+		// Decrease number of custommers.
+		data->custommers -= 1;
+		// Get time from custommer.
+		int time = data->times[data->custommers];
+		// Release the kraken.
+		up(data->semaphores, SEM_BARBER);
+		// Leave critical section.
+		up(data->semaphores, SEM_MUTEX);
+
+		// Cut custommer's hair.
+		cut_hair(time);
+
+		// Run closing procedure.
+		up(data->semaphores, SEM_CUTTED);
+		down(data->semaphores, SEM_SITTING);
+	}
+	pthread_exit((void *) 0);
+}
 
 int main(const int argc, const char* argv[])
 {
@@ -16,6 +65,10 @@ int main(const int argc, const char* argv[])
 
 	// Process program arguments.
 	process_args(&s, argv, argc);
+
+	pthread_t tid;
+	pthread_create(&tid, NULL, barber, NULL);
+	pthread_detach(tid);
 
 	// Initialize server socket and bind it.
 	if (server_init(&s, s.port) == -1) {
